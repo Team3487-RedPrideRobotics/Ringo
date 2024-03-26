2019c2e933cdef69667d33b099c1108a1fb1f64e
3487 Red Pride Robotics <team3487@gmail.com>
2024-03-26 14:59:47 -0400

    Auto Drive Failsafe

--- src/main/java/frc/robot/commands/Autonomoose/AutoDriveStraight.java
+++ src/main/java/frc/robot/commands/Autonomoose/AutoDriveStraight.java
@@ -8,15 +8,19 @@ public class AutoDriveStraight extends AutonomousCommand {
     private double _distance;
     private double _drive_speed;
     private boolean done;
+    private double pastEncoder;
+    private int counter;
 
     public AutoDriveStraight(DriveSubsystem drive, double distance, double drive_speed)
     {
         m_drive = drive;
         _distance = distance;
         _drive_speed = drive_speed;
+        counter = 0;
     }
      @Override
     public void execute() {
+    pastEncoder = m_drive.getLeftDriveEncoder();
     System.out.println("Left Encoder:" + m_drive.getLeftDriveEncoder());
         if(_distance >= 0){
             if(m_drive.getLeftDriveEncoder() >= _distance ){
@@ -39,8 +43,11 @@ public class AutoDriveStraight extends AutonomousCommand {
     
     @Override
     public boolean isFinished() {
+        if(pastEncoder == m_drive.getLeftDriveEncoder()) counter++;
+        if(counter > 5) done = true;
         if(done) m_drive.resetEncoders();
         return done;
+        
     }
 
     @Override
